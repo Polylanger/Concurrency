67e6d5928988366bd3a87fddc3d88484a2fa1702
Polylanger <1395403761@qq.com>
2018-07-14 10:36:50 +0800

    project init

---
+++ src/main/java/com/cmft/qz/Concurrency/ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.cmft.qz.Concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+}
