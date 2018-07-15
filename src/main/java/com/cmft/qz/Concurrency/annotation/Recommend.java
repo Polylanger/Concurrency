e5ae7fed0d393c9e9506d4e696ebb417e7ac5c35
Polylanger <1395403761@qq.com>
2018-07-15 13:27:36 +0800

    Add 4 annotation

---
+++ src/main/java/com/cmft/qz/Concurrency/annotation/Recommend.java
@@ -0,0 +1,15 @@
+package com.cmft.qz.Concurrency.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(SOURCE)
+@Target(TYPE)
+public @interface Recommend {
+
+	String value() default "";
+	
+}
